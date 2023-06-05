1f302f429a574e7bce1ec807799de273015bb9dc
MichalK <95657053+michalK00@users.noreply.github.com>
2023-06-06 00:11:57 +0200

    added concept models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# It's still just a concept
+# These were not migrated yet
+
+class Task(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, max_length=11)
+    name = models.CharField(max_length=25)
+    source = models.CharField(max_length=25)
+    task_content = models.CharField(max_length=500)
+
+
+class UserTask(models.Model):
+    task_id = models.ForeignKey(Task)
+    user_id = models.ForeignKey(User)
+    user_note = models.CharField(max_length=500)
+    finished_date = models.DateTimeField()
