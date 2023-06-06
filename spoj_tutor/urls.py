736a35c1c7bd78412feb276c51605d9a3499bec7
MichalK <95657053+michalK00@users.noreply.github.com>
2023-06-07 00:47:58 +0200

    added bootstrap and some basic styling to home.html

--- spoj_tutor/urls.py
+++ spoj_tutor/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from tasks import  views
+from tasks import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
