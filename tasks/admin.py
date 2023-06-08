ca4db794f24987e82f75aaebdf858a3d7eed2a10
DudasOn <pawel200216@interia.eu>
2023-06-08 19:24:25 +0200

    Started implementing CSV reading for quick database expansion

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,10 @@
+from django.template.response import TemplateResponse
 from django.contrib import admin
-from .models import Task
+from django.urls import path
+
+from .models import Task, Spoj
+import csv
 
 # Register your models here.
 admin.site.register(Task)
+admin.site.register(Spoj)
