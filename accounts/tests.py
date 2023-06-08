792800eb8c4bcfe0b240ea89c457c9c717d8a6db
MichalK <95657053+michalK00@users.noreply.github.com>
2023-06-08 18:32:22 +0200

    added navbar, signup view, and js from bootstrap

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import signup
+
 
 # Create your tests here.
+class SignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_view_success_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
