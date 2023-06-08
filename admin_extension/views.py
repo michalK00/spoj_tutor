e6f8551d765e01511586103eb8d36a0dadcaf3dd
MichalK <95657053+michalK00@users.noreply.github.com>
2023-06-08 23:42:10 +0200

    added admin authorization requirement for add_tasks view

--- admin_extension/views.py
+++ admin_extension/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import AddFromCSVForm
+from django.contrib.admin.views.decorators import staff_member_required
 # Create your views here.
 
 
+@staff_member_required()
 def add_tasks_from_csv(request):
     if request.method == "POST":
         form = AddFromCSVForm(request.POST, request.FILES)
