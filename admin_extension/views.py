c59d8a2ee7f8f4c59b3b6368e05a30e88b303ddf
DudasOn <pawel200216@interia.eu>
2023-06-09 18:23:50 +0200

    Reformatted the file so the indentation is more manageable

--- admin_extension/views.py
+++ admin_extension/views.py
@@ -9,31 +9,34 @@ from tasks.models import Task
 # Create your views here.
 @staff_member_required()
 def add_tasks_from_csv(request):
-    if request.method == "POST":
-        form = AddFromCSVForm(request.POST, request.FILES)
-        if form.is_valid():  # <- file is validated, so we can just use it as is
-            # Adding operation is one transaction
-            with transaction.atomic():
-                file = request.FILES.get("file")
-                file.seek(0)
-                lines = file.readlines()
-                csv_fields = lines[0].decode().strip().split(",")
-                csv_fields.append("spoj")
+    def return_form():
+        return render(request, "admin/add_tasks_csv.html", {"form": form})
 
-                for line in lines[1:]:
-                    fields = line.decode().replace("\r\n", "").split(",")
-
-                    fields.append(choices[int(form.cleaned_data["spoj"])][1])
-
-                    normalized_fields = [
-                        None if elem == "" else elem for elem in fields
-                    ]
-
-                    data = dict(zip(csv_fields, normalized_fields))
-                    Task.objects.create(**data)
-
-            return redirect("admin:index")
-    else:
+    if request.method != "POST":
         form = AddFromCSVForm()
+        return return_form()
+
+    form = AddFromCSVForm(request.POST, request.FILES)
+    if not form.is_valid():  # <- file is validated, so we can just use it as is
+        return return_form()
+
+    # Adding operation is one transaction
+    with transaction.atomic():
+        file = request.FILES.get("file")
+        file.seek(0)
+
+        lines = file.readlines()
+
+        csv_fields = lines[0].decode().strip().split(",")
+        csv_fields.append("spoj")
+
+        for line in lines[1:]:
+            fields = line.decode().replace("\r\n", "").split(",")
+            fields.append(choices[int(form.cleaned_data["spoj"])][1])
+
+            normalized_fields = [None if elem == "" else elem for elem in fields]
+
+            data = dict(zip(csv_fields, normalized_fields))
+            Task.objects.create(**data)
+    return redirect("admin:index")
     # TODO: add file parsing, database modification and tests
-    return render(request, "admin/add_tasks_csv.html", {"form": form})
